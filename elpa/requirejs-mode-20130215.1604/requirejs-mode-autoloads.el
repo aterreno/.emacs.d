0e81d121182ff9c6c6de2b9a4fe89641703e37e9
Antonio Terreno <antonio.terreno@gmail.com>
2013-08-30 10:17:16 +0100

    updated

---
+++ elpa/requirejs-mode-20130215.1604/requirejs-mode-autoloads.el
@@ -0,0 +1,18 @@
+;;; requirejs-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("requirejs-mode-pkg.el" "requirejs-mode.el")
+;;;;;;  (20987 27434 350325))
+
+;;;***
+
+(provide 'requirejs-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; requirejs-mode-autoloads.el ends here
